8394b0e07b40b0876838c61897eb4d5c266aaf01
Zakk Hoyt <zacharyhoyt@zakkimac.local>
2014-06-08 12:49:23 -0700

    Initial Commit

---
+++ BusSnooze/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BusSnooze
+//
+//  Created by Zakk Hoyt on 6/8/14.
+//  Copyright (c) 2014 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
